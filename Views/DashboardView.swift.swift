6f1972f1f31d00ad714af1633716d6d23be9da9e
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-10-07 10:28:45 -0400

    Create Dashboard screen (Issue #3)

---
+++ Views/DashboardView.swift.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardView.swift.swift
+//  CleanTrack
+//
+//  Created by Jose Flores on 2025-10-07.
+//
+
+import SwiftUI
+
+struct DashboardView_swift: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardView_swift()
+}
